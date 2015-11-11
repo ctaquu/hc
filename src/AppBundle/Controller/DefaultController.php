d117516abea3ac7daf848982892d9f932d3164cb
damjan <ctaquu@gmail.com>
2015-11-12 00:42:30 +0100

    night one...

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,4 +14,20 @@ class DefaultController extends Controller
     {
         return $this->render('default/index.html.twig');
     }
+
+    /**
+     * @Route("trips", name="trips")
+     */
+    public function tripsAction()
+    {
+        return $this->render('default/trips.html.twig');
+    }
+
+    /**
+     * @Route("new", name="new trip")
+     */
+    public function newAction()
+    {
+        return $this->render('default/new.html.twig');
+    }
 }
